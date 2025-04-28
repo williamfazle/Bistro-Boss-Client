d823792a713d2e5966da9ab9a8311bb21fe529a2
william-fazle <williamfazle@gmail.com>
2025-04-28 19:43:21 +0600

    Added review and testimonials

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,12 +4,12 @@ import './Featured.css'
 
 const Featured = () => {
   return (
-    <div className="featured-item text-white pt-8 my-20">
+    <div className="featured-item bg-fixed text-white pt-8 my-20">
           <SectionTitle
               subHeading="check it out" heading="Featured Item"
           ></SectionTitle>
 
-          <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+          <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-60 pb-20 pt-12 px-36">
               <div>
                   <img src={featuredImg} alt="" />
               </div>
@@ -17,7 +17,7 @@ const Featured = () => {
                   <p>Aug 20, 2029</p>
                   <p className="uppercase">Where can i get some?</p>
                   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur vel quaerat ea! Dolor quibusdam eaque doloremque odio vel quos blanditiis fuga ex hic, debitis illum quis ipsam quas, rerum doloribus vitae, autem quisquam! Cumque excepturi distinctio laudantium debitis? Eos voluptas nesciunt sit eum facilis beatae reiciendis est minus ex hic?</p>
-                  <button className="btn btn-outline">Order Now</button>
+                  <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
               </div>
           </div>
       </div>
