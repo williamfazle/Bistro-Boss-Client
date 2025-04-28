36cf52cc456a93057d9215c08605a72d935a3263
william-fazle <williamfazle@gmail.com>
2025-04-28 14:37:10 +0600

    Added some new featured

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,28 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle"
+import featuredImg from '../../../assets/home/featured.jpg'
+import './Featured.css'
+
+const Featured = () => {
+  return (
+    <div className="featured-item text-white pt-8 my-20">
+          <SectionTitle
+              subHeading="check it out" heading="Featured Item"
+          ></SectionTitle>
+
+          <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+              <div>
+                  <img src={featuredImg} alt="" />
+              </div>
+              <div className="md:ml-10">
+                  <p>Aug 20, 2029</p>
+                  <p className="uppercase">Where can i get some?</p>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur vel quaerat ea! Dolor quibusdam eaque doloremque odio vel quos blanditiis fuga ex hic, debitis illum quis ipsam quas, rerum doloribus vitae, autem quisquam! Cumque excepturi distinctio laudantium debitis? Eos voluptas nesciunt sit eum facilis beatae reiciendis est minus ex hic?</p>
+                  <button className="btn btn-outline">Order Now</button>
+              </div>
+          </div>
+      </div>
+    
+  )
+}
+
+export default Featured
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from "../Banner/Banner"
 import Catagory from "../Catagory/Catagory"
+import Featured from "../Featured/Featured"
+import PopularMenu from "../PopularMenu/PopularMenu"
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Catagory></Catagory>
+      <PopularMenu></PopularMenu>
+      <Featured></Featured>
     </div>
   )
 }
